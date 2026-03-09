487b4e85400ba70ae8208d3998d1a13b033ac122
Luke Street <luke@street.dev>
2026-03-09 01:28:23 -0600

    Remove commented code

--- libs/JSystem/src/JKernel/JKRHeap.cpp
+++ libs/JSystem/src/JKernel/JKRHeap.cpp
@@ -593,14 +593,6 @@ void* operator new[](size_t size) {
     return JKRHeap::alloc(size, 4, NULL);
 }
 #else
-// void* operator new[](size_t size JKR_HEAP_TOKEN_PARAM) {
-//     return fallback_alloc(size, 0, false);
-// }
-
-// void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
-//     return fallback_alloc(size, 0, false);
-// }
-
 void* operator new[](size_t size JKR_HEAP_TOKEN_PARAM) {
     void* mem = JKRHeap::alloc(size, alignof(max_align_t), NULL);
     if (mem == NULL) {
