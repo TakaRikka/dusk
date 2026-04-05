d47927f2eccf35974c3ff21644bf6b6dea13adef
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2026-04-05 16:58:46 +0300

    Improve formatting and custom variable names

--- src/d/actor/d_a_alink_dusk.cpp
+++ src/d/actor/d_a_alink_dusk.cpp
@@ -5,59 +5,68 @@
 #include "d/d_meter2_info.h"
 #include "m_Do/m_Do_audio.h"
 
-bool armorQuickToggleVar1 = false;
-bool armorQuickToggleVar2 = false;
-uint32_t countyer = 0;
-uint32_t armorQuickToggleTimer1 = 0;
-uint32_t armorQuickToggleTimer2 = 0;
+bool armorQuickToggleCooldown = false;
+bool armorQuickToggleInitiated = false;
+u32 armorQuickToggleCounter = 0;
+u32 armorQuickToggleInitiatedTimer = 0;
+u32 armorQuickToggleCooldownTimer = 0;
 
 void daAlink_c::handleArmorsQuickToggle() {
-    if (!dusk::getSettings().game.enableArmorsQuickToggle) return;
-
-    // temporary if statement code for automatically having magic and zora armors unlocked to test the mod
-    if (!dComIfGs_isItemFirstBit(dItemNo_WEAR_ZORA_e)) {
-        dComIfGs_onItemFirstBit(dItemNo_WEAR_ZORA_e); dComIfGs_onItemFirstBit(dItemNo_ARMOR_e);
+    if (!dusk::getSettings().game.enableArmorsQuickToggle) {
+        return;
     }
 
     // Have magic and zora armors
-    if (!dComIfGs_isItemFirstBit(dItemNo_WEAR_ZORA_e) && !dComIfGs_isItemFirstBit(dItemNo_ARMOR_e)) return;
+    if (!dComIfGs_isItemFirstBit(dItemNo_WEAR_ZORA_e) && !dComIfGs_isItemFirstBit(dItemNo_ARMOR_e)) {
+        return;
+    }
 
     // Be Human
-    if (dComIfGs_getTransformStatus()) return;
+    if (dComIfGs_getTransformStatus()) {
+        return;
+    }
 
-    if (!armorQuickToggleVar1 && mDoCPd_c::getTrigDown(PAD_1) && countyer < 2)
+    if (!armorQuickToggleCooldown && mDoCPd_c::getHoldR(PAD_1) && mDoCPd_c::getTrigX(PAD_1) && armorQuickToggleCounter < 2)
     {
-        armorQuickToggleVar2 = true;
-        countyer++;
+        armorQuickToggleInitiated = true;
+        armorQuickToggleCounter++;
     }
 
-    if (armorQuickToggleVar2) {
-        armorQuickToggleTimer1++;
-        if (armorQuickToggleTimer1 >= 15) {
-            armorQuickToggleVar2 = false;
-            armorQuickToggleVar1 = true;
-            armorQuickToggleTimer1 = 0;
-            armorQuickToggleTimer2 = 0;
+    if (armorQuickToggleInitiated) {
+        armorQuickToggleInitiatedTimer++;
+        if (armorQuickToggleInitiatedTimer >= 15) {
+            armorQuickToggleInitiated = false;
+            armorQuickToggleCooldown = true;
+            armorQuickToggleInitiatedTimer = 0;
+            armorQuickToggleCooldownTimer = 0;
             setClothesChange(0);
-            mDoAud_seStart(0x4F, 0, 0, 0);
-            if (countyer == 1) {
-                if (dComIfGs_getSelectEquipClothes() != dItemNo_WEAR_ZORA_e) dComIfGs_setSelectEquipClothes(dItemNo_WEAR_ZORA_e);
-                else dComIfGs_setSelectEquipClothes(dItemNo_WEAR_KOKIRI_e);
-            } else if (countyer == 2 && dComIfGs_isItemFirstBit(dItemNo_ARMOR_e)) {
-                if (dComIfGs_getSelectEquipClothes() != dItemNo_ARMOR_e) dComIfGs_setSelectEquipClothes(dItemNo_ARMOR_e);
-                else dComIfGs_setSelectEquipClothes(dItemNo_WEAR_KOKIRI_e);
+            mDoAud_seStart(Z2SE_SY_ITEM_SET_X, 0, 0, 0);
+            if (armorQuickToggleCounter == 1) {
+                if (dComIfGs_getSelectEquipClothes() != dItemNo_WEAR_ZORA_e) {
+                    dComIfGs_setSelectEquipClothes(dItemNo_WEAR_ZORA_e);
+                }
+                else {
+                    dComIfGs_setSelectEquipClothes(dItemNo_WEAR_KOKIRI_e);
+                }
+            } else if (armorQuickToggleCounter == 2 && dComIfGs_isItemFirstBit(dItemNo_ARMOR_e)) {
+                if (dComIfGs_getSelectEquipClothes() != dItemNo_ARMOR_e) {
+                    dComIfGs_setSelectEquipClothes(dItemNo_ARMOR_e);
+                }
+                else {
+                    dComIfGs_setSelectEquipClothes(dItemNo_WEAR_KOKIRI_e);
+                }
             }
-            countyer = 0;
-        }
-    }
-    if (armorQuickToggleVar1) {
-        armorQuickToggleTimer2++;
-        if (armorQuickToggleTimer2 >= 20) {
-            armorQuickToggleVar1 = false;
-            armorQuickToggleTimer2 = 0;
+            armorQuickToggleCounter = 0;
         }
     }
 
+    if (armorQuickToggleCooldown) {
+        armorQuickToggleCooldownTimer++;
+        if (armorQuickToggleCooldownTimer >= 20) {
+            armorQuickToggleCooldown = false;
+            armorQuickToggleCooldownTimer = 0;
+        }
+    }
 }
 
 void daAlink_c::handleQuickTransform() {
