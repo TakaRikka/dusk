60a3b583aadbfd3fb11c4d08e5b8b9df03c6447a
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2026-04-18 17:30:28 +0300

    Added custom vars for custom meter colors

--- include/dusk/settings.h
+++ include/dusk/settings.h
@@ -5,6 +5,17 @@
 
 namespace dusk {
 
+struct MeterColorsOverride {
+    GXColor lanternCustomTop = {230, 170, 0, 255};
+    GXColor lanternCustomBottom = {255, 255, 140, 255};
+    GXColor oxygen1CustomBottom = {200, 200, 255, 255};
+    GXColor oxygen1CustomTop = {80, 180, 255, 255};
+    GXColor oxygen2CustomBottom = {255, 100, 100, 255};
+    GXColor oxygen2CustomTop = {255, 10, 10, 255};
+};
+
+extern MeterColorsOverride s_meterColorsOverride;
+
 using namespace config;
 
 enum class BloomMode : int {
