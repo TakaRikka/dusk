de6db5e3794f72889f5a5627ba69ad2456ca3f1a
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2026-04-18 09:24:01 +0300

    Custom Meter Colors 5

--- src/dusk/imgui/ImGuiMenuTools.cpp
+++ src/dusk/imgui/ImGuiMenuTools.cpp
@@ -54,6 +54,7 @@ namespace dusk {
             ImGui::MenuItem("State Share", hotkeys::SHOW_STATE_SHARE, &m_showStateShare);
             ImGui::MenuItem("Debug Camera", hotkeys::SHOW_DEBUG_CAMERA, &m_showCameraOverlay);
             ImGui::MenuItem("Audio Debug", hotkeys::SHOW_AUDIO_DEBUG, &m_showAudioDebug);
+            ImGui::MenuItem("Meter Colors", nullptr, &m_showMeterColors);
             ImGui::MenuItem("Bloom", nullptr, &m_showBloomWindow);
             ImGui::MenuItem("Stub Log", nullptr, &m_showStubLog);
 
