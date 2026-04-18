480f880b7648fa54519a52f624d027c2a06f3e9c
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2026-04-18 09:22:28 +0300

    Custom Meter Colors 3

--- src/dusk/imgui/ImGuiBloomWindow.hpp
+++ src/dusk/imgui/ImGuiBloomWindow.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
 namespace dusk {
+struct MeterColorsOverride {
+    GXColor lanternCustomTop = {230, 170, 0, 255};
+    GXColor lanternCustomBottom = {255, 255, 140, 255};
+    GXColor oxygen1CustomBottom = {200, 200, 255, 255};
+    GXColor oxygen1CustomTop = {80, 180, 255, 255};
+    GXColor oxygen2CustomBottom = {255, 100, 100, 255};
+    GXColor oxygen2CustomTop = {255, 10, 10, 255};
+};
+
+extern MeterColorsOverride s_meterColorsOverride;
+void DrawMeterColorsWindow(bool& open);
 void DrawBloomWindow(bool& open);
 void ApplyBloomOverride();
 }  // namespace dusk
