3cac29891fc8adfc3a958644c34d24a8b6669066
PJB3005 <pieterjan.briers+git@gmail.com>
2026-05-14 01:55:04 +0200

    Use safe string copies in d_save.cpp
    Seeing sentry crashes here that I suspect involve buffer overflows here.

--- src/d/d_save.cpp
+++ src/d/d_save.cpp
@@ -31,6 +31,9 @@
 #include "dusk/settings.h"
 #include <f_ap/f_ap_game.h>
 #include <dusk/autosave.h>
+
+#include "dusk/string.hpp"
+#define strcpy dusk::SafeStringCopy
 #endif
 
 static u8 dSv_item_rename(u8 i_itemNo) {
