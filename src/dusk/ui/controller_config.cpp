aa178a211203250fe67a61f2a24d13d8472e4c1c
gymnast86 <stock.joann@gmail.com>
2026-05-12 17:37:17 -0700

    more explanation text

--- src/dusk/ui/controller_config.cpp
+++ src/dusk/ui/controller_config.cpp
@@ -933,8 +933,9 @@ void ControllerConfigWindow::render_page(Pane& pane, int port, Page page) {
         SDL_Gamepad* gamepad = gamepad_for_port(port);
         pane.add_section("Custom Action Bindings");
         pane.add_text("A button bound to any action here will REPLACE the default control for"
-                      " that action. Only bind buttons here that aren't used anywhere else. This is "
-                      "not recommended for regular Gamecube controllers.");
+                      " that action. Only bind buttons here that aren't used anywhere else. The glyphs"
+                      " shown for in game actions will not change. This is not recommended for "
+                      " regular Gamecube controllers.");
         auto addActionBinding = [&](auto actionBind, const std::string& key) {
             pane.add_select_button({
                            .key = key,
