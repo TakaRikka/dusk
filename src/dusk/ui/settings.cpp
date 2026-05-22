1a6b991c0e36e48ff6903b0123f37c07df3e23b4
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2026-05-22 09:45:46 +0200

    fix: add kTextureReplacementHelpText

--- src/dusk/ui/settings.cpp
+++ src/dusk/ui/settings.cpp
@@ -376,6 +376,8 @@ const Rml::String kDepthOfFieldHelpText =
 const Rml::String kUnlockFramerateHelpText =
     "<br/>Uses inter-frame interpolation to enable higher frame rates.<br/><br/>May introduce minor "
     "visual artifacts or animation glitches.";
+const Rml::String kTextureReplacementHelpText =
+    "Enable installed texture replacements.";
 
 int float_setting_percent(ConfigVar<float>& var) {
     return static_cast<int>(var.getValue() * 100.0f + 0.5f);
