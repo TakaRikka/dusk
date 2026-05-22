9c672d1ac454a45a98294b0caeee0c4a9dbdc4a6
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2026-05-22 22:31:13 +0200

    fix: rename ActionBindType enum

--- src/dusk/ui/controller_config.cpp
+++ src/dusk/ui/controller_config.cpp
@@ -939,14 +939,14 @@ void ControllerConfigWindow::render_page(Pane& pane, int port, Page page) {
 
             pane.add_section("Gameplay");
             for (auto& [configVars, actionName, type] : getActionBinds() | std::views::values) {
-                if (type == Type::GAMEPLAY) {
+                if (type == ActionBindType::GAMEPLAY) {
                     addActionBinding(&configVars->at(port), actionName);
                 }
             }
 
             pane.add_section("Interface");
             for (auto& [configVars, actionName, type] : getActionBinds() | std::views::values) {
-                if (type == Type::INTERFACE) {
+                if (type == ActionBindType::INTERFACE) {
                     addActionBinding(&configVars->at(port), actionName);
                 }
             }
@@ -992,14 +992,14 @@ void ControllerConfigWindow::render_page(Pane& pane, int port, Page page) {
 
         pane.add_section("Gameplay");
         for (auto& [configVars, actionName, type] : getActionBinds() | std::views::values) {
-            if (type == Type::GAMEPLAY) {
+            if (type == ActionBindType::GAMEPLAY) {
                 addActionBinding(&configVars->at(port), actionName);
             }
         }
 
         pane.add_section("Interface");
         for (auto& [configVars, actionName, type] : getActionBinds() | std::views::values) {
-            if (type == Type::INTERFACE) {
+            if (type == ActionBindType::INTERFACE) {
                 addActionBinding(&configVars->at(port), actionName);
             }
         }
