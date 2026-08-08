46fbf1120139ad966f7b2e864549f1fb63c72a86
baxlek <115663151+baxlek@users.noreply.github.com>
2026-08-08 18:46:03 -0500

    Fix syntax error in settings.cpp

--- src/dusk/ui/settings.cpp
+++ src/dusk/ui/settings.cpp
@@ -1226,7 +1226,7 @@ SettingsWindow::SettingsWindow(bool prelaunch) : mPrelaunch(prelaunch) {
         addOption("Quick Transform (R+Y)", getSettings().game.enableQuickTransform,
             "Transform instantly by pressing R and Y simultaneously.");
         addOption("Aiming Reticle", getSettings().game.aimingReticle,
-            "Shows an aiming reticle for bow and slingshot."
+            "Shows an aiming reticle for bow and slingshot.");
 
         leftPane.add_section("Speedrunning");
         config_bool_select(leftPane, rightPane, getSettings().game.speedrunMode,
